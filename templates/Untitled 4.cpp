94a2fd816ccf10e7ba9a804af8db206811ce03c0
nicmasaribeiro <46984245+nicmasaribeiro@users.noreply.github.com>
2025-08-30 19:12:43 -0300

    Add files via upload

---
+++ templates/Untitled 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace::std;
+
+int function(int i){
+	cout << i << endl;
+}
+using namespace std;
+int main(int argc, char *argv[]) {
+
+		cout << "hell" << endl;
+		function(1);
+		function(2);
+		
+}
